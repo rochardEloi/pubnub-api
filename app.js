8ea9b065dae80ab1ff84610531bbea7fc2319dfc
Rochard Eliamar Eloi <rochardeliamar.eloi@uniq.edu>
2022-04-11 21:31:33 -0400

    Update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
 app.use(bodyParser.json());
 
 app.use("/", pubApp);
